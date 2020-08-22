70181098d44aae58d5c8e984f5f4f8931ef6c5c7
William Stephens <william.a.stephens17@gmail.com>
2020-08-22 17:09:50 -0500

    Similar data types

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,6 +6,15 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            string phrase = "this is a phrase";
+            char a = 'a';
+            int firstInt = 5342;
+            firstInt = 3;
+            Console.WriteLine(firstInt);
+            Console.WriteLine("Hello World?");
+            Console.WriteLine("  /");
+            Console.WriteLine(" /");
+            Console.WriteLine("/");
             Console.WriteLine("Hello World!");
         }
     }
