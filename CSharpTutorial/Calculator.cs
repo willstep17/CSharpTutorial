43d21057a8e8bb723003ad550aa6eb1f2453de5a
William Stephens <william.a.stephens17@gmail.com>
2020-08-22 17:58:14 -0500

    Calculator

---
+++ CSharpTutorial/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharpTutorial
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+
+}
--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,22 +6,17 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
-            string phrase = "this is a phrase";
-            char a = 'a';
-            int firstInt = 5342;
-            int[] luckyNumbers = { 17, 55, 33, 1111 };
-            foreach (int i in luckyNumbers)
-            {
-                Console.WriteLine(i);
-            }
-            firstInt = 3;
-            Console.WriteLine("ReBuildAllCommand: Ctrl-Command-B");
-            Console.WriteLine(firstInt);
-            Console.WriteLine("Hello World?");
-            Console.WriteLine("  /");
-            Console.WriteLine(" /");
-            Console.WriteLine("/");
-            Console.WriteLine("Hello World!");
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Add(3, 5));
+            Console.WriteLine("new");
         }
+
+    static void sayHi()
+    {
+        Console.WriteLine("Hello from sayHi");
     }
+
+    }
+
+
 }
