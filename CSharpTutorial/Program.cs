1255da1c15d7675103f64a666923f31a5b581579
William Stephens <william.a.stephens17@gmail.com>
2020-08-22 17:16:43 -0500

    Visual Studio Shortcuts

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -9,7 +9,13 @@ namespace CSharpTutorial
             string phrase = "this is a phrase";
             char a = 'a';
             int firstInt = 5342;
+            int[] luckyNumbers = { 17, 55, 33, 1111 };
+            foreach (int i in luckyNumbers)
+            {
+                Console.WriteLine(i);
+            }
             firstInt = 3;
+            Console.WriteLine("ReBuildAllCommand: Ctrl-Command-B");
             Console.WriteLine(firstInt);
             Console.WriteLine("Hello World?");
             Console.WriteLine("  /");
