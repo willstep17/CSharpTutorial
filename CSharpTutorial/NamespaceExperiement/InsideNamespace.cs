22e79925d07faa084442610e09c493a8a2b9a870
William Stephens <william.a.stephens17@gmail.com>
2020-08-24 13:48:19 -0500

    NamespaceExperiment

---
+++ CSharpTutorial/NamespaceExperiement/InsideNamespace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CSharpTutorial.NamespaceExperiement
+{
+    public class InsideNamespace
+    {
+        public InsideNamespace()
+        {
+        }
+
+        public static void SayHiFromInsideNamespace()
+        {
+            Console.WriteLine("Hello from InsideNamespace");
+        }
+    }
+}
